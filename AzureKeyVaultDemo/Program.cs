45ae3830c7f29933dcbea042b749c10ae39babc1
Sameer Khare <sameer.khare@globant.com>
2019-09-16 18:22:26 +0530

    Add project files.

---
+++ AzureKeyVaultDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Extensions.Logging;
+
+namespace AzureKeyVaultDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>();
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+                   .ConfigureAppConfiguration((context, config) =>
+                   {
+                       var builtConfig = config.Build();
+                       config.AddAzureKeyVault(
+                           $"https://{builtConfig["azureKeyVault:vault"]}.vault.azure.net/",
+                           builtConfig["azureKeyVault:clientId"],
+                           builtConfig["azureKeyVault:clientSecret"],
+                           new DefaultKeyVaultSecretManager());
+                   })
+                   .UseStartup<Startup>();
+
+    }
+}
+// $"https://{builtConfig["azureKeyVault:vault"]}.vault.azure.net/",
+//builtConfig["azureKeyVault:clientId"],
+//                          builtConfig["azureKeyVault:clientSecret"],    
